e47aaee491839077693cecd7d93d38a019305e56
River Marmorstein <river@rmarmorstein.com>
2015-02-23 22:42:20 -0500

    primitive config and ticket objects

---
+++ src/main/java/net/ghostrealms/ticket/Ticket.java
@@ -0,0 +1,9 @@
+package net.ghostrealms.ticket;
+
+/**
+ * Created by River on 2/23/2015.
+ * Ticket Object Class* 
+ */
+
+public class Ticket {
+}
---
+++ src/main/java/net/ghostrealms/ticket/TicketManager.java
@@ -0,0 +1,9 @@
+package net.ghostrealms.ticket;
+
+/**
+ * Created by River on 2/23/2015.
+ * Ticket Manager for Loading, caching, and updating tickets throughout the database system* 
+ */
+
+public class TicketManager {
+}
