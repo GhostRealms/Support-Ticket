e9c08f4ce008d3cda436446201bfd6261b081b18
River Marmorstein <river@rmarmorstein.com>
2015-02-23 22:41:46 -0500

    add enable and disable methods

--- src/main/java/net/ghostrealms/ticket/SupportTicket.java
+++ src/main/java/net/ghostrealms/ticket/SupportTicket.java
@@ -1,7 +1,23 @@
 package net.ghostrealms.ticket;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by River on 2/23/2015.
+ * Main Class for the Support Tickets Plugin *
+ * Licensed under Apache 2 *
+ * Originally Created for the Ghost Realms server* 
  */
-public class SupportTicket {
+public class SupportTicket extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+
+    }
+    
 }
