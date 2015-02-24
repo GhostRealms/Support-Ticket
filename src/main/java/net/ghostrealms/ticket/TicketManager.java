991c16ecc2e0e5fc9a45b6a04f34bddd416f01da
River Marmorstein <river@rmarmorstein.com>
2015-02-23 23:47:59 -0500

    Ticket Sessions for users to simplify commands, not having to specify the 5 character ticket id for every command

--- src/main/java/net/ghostrealms/ticket/TicketManager.java
+++ src/main/java/net/ghostrealms/ticket/TicketManager.java
@@ -1,5 +1,10 @@
 package net.ghostrealms.ticket;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
 /**
  * Created by River on 2/23/2015.
  * Ticket Manager for Loading, caching, and updating tickets throughout the database system* 
@@ -8,9 +13,55 @@ package net.ghostrealms.ticket;
 public class TicketManager {
     
     private final DataManager manager;
+    private Random random = new Random(System.currentTimeMillis());
+    private Map<UUID, Integer> sessions = new HashMap<>();
+    
+    //Stored Objects
+    private int lastNumber;
+    
+    //Configuration values
+    private boolean sequential = false;
     
     public TicketManager(DataManager manager) {
         this.manager = manager;
     }
+    
+    public Ticket createTicket() {
+        return null;
+    }
+    
+    public void saveTicket() {
+        
+    }
+    
+    public Ticket updateTicket() {
+        return null;
+    }
+    
+    public int generateTicketNumber() {
+        //TODO check for duplicate numbers
+        return random.nextInt(5);
+    }
+    
+    protected Map<UUID, Integer> getAllSessions() {
+        return sessions;
+    }
+    
+    public void createSession(UUID user, Integer ticketId) {
+        sessions.put(user, ticketId);
+    }
+    
+    public void expireSession(UUID id) {
+        sessions.remove(id);
+    }
+    
+    public void updateSession(UUID user, int newSession) {
+        sessions.remove(user);
+        sessions.put(user, newSession);
+    }
+    
+    
+    
+    
     
 }
