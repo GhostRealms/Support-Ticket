12e27642f4abf2f355181a82721cfe27838884ac
River Marmorstein <river@rmarmorstein.com>
2015-02-25 23:53:24 -0500

    add some setters and getters

--- src/main/java/net/ghostrealms/ticket/Ticket.java
+++ src/main/java/net/ghostrealms/ticket/Ticket.java
@@ -1,6 +1,8 @@
 package net.ghostrealms.ticket;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import java.util.Map;
 import java.util.UUID;
@@ -13,7 +15,7 @@ import java.util.UUID;
 public class Ticket {
     
     private int id;
-    private int tstamp;
+    private long tstamp;
     private Location location;
     private String issue;
     private Map<Integer, String> comments;
@@ -28,6 +30,50 @@ public class Ticket {
         creator = owner;
         this.location = location;
         this.issue = issue;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public Location getLocation() {
+        return location;
+    }
+    
+    public String getIssue() {
+        return issue;
+    }
+    
+    public UUID getOwner() {
+        return creator;
+    }
+    
+    public UUID getAssignedStaff() {
+        return assigned;
+    }
+    
+    public long getTimestamp() {
+        return tstamp;
+    }
+    
+    public void teleport(Player user) {
+        user.teleport(location);
+    }
+    
+    public void setAssignedMember(UUID staff) {
+        this.assigned = staff;
+    }
+    
+    public void addComment(String comment) {
+        long time = System.currentTimeMillis();
+    }
+    
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+    
+    public void update() {
+        //TODO query the database using the DataManager and update the ticket values
     }
     
 }
